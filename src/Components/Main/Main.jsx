37516e6e5f749a073dc493bfa75feb415b8e92e2
Ahmed-eldaker <107445059+Ahmed-eldaker@users.noreply.github.com>
2024-04-23 18:33:07 +0200

    Update Main.jsx

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,12 +1,12 @@
 import Navbar from '../Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Footer/Footer'
-
+import Home from "../Home/Home";
 const Main = () => {
   return (
     <div>
         <Navbar/>
-        <Outlet/>
+        <Home/>
         <Footer/>
     </div>
   )
